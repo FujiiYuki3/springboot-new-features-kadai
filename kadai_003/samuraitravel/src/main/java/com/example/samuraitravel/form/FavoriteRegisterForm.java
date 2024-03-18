705875af3afa0857a8ce9d71731bb33713693fe2
FujiiYuki3 <151428461+FujiiYuki3@users.noreply.github.com>
2024-03-18 09:50:44 +0900

    再プッシュ

--- kadai_003/samuraitravel/src/main/java/com/example/samuraitravel/form/FavoriteRegisterForm.java
+++ kadai_003/samuraitravel/src/main/java/com/example/samuraitravel/form/FavoriteRegisterForm.java
@@ -1,8 +1,5 @@
 package com.example.samuraitravel.form;
 
-import com.example.samuraitravel.entity.House;
-import com.example.samuraitravel.entity.User;
-
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -14,20 +11,12 @@ public class FavoriteRegisterForm {
 	@NotNull
 	private Integer userId;
 	
-	public House getHouse(House house) {
-		return house;
+	public void setHouseId(Integer houseId) {
+		this.houseId = houseId;
 	}
 	
-	public void setHouseId(House house) {
-		this.houseId = house.getId();
-	}
-	
-	public User getUser(User user) {
-		return user;
-	}
-	
-	public void setUserId(User user) {
-		this.userId = user.getId();
+	public void setUserId(Integer userId) {
+		this.userId = userId;
 	}
 
 }
