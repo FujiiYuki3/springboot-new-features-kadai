66637d729b4308c1c9610b9435df5577932a87d5
FujiiYuki3 <151428461+FujiiYuki3@users.noreply.github.com>
2024-03-26 15:10:44 +0900

    kadai_003をプッシュ

---
+++ kadai_003/samuraitravel/src/main/java/com/example/samuraitravel/entity/Favorite.java
@@ -0,0 +1,47 @@
+package com.example.samuraitravel.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "favorites")
+public class Favorite {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	@ManyToOne
+	@JoinColumn(name = "house_id")
+	private House house;
+	
+	public User getUser() {
+		return user;
+	}
+	
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	public House getHouse() {
+		return house;
+	}
+	
+	public void setHouse(House house) {
+		this.house = house;
+	}
+
+}
---
+++ kadai_003/samuraitravel/src/main/java/com/example/samuraitravel/form/ReviewForm.java
@@ -0,0 +1,13 @@
+/*package com.example.samuraitravel.form;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class ReviewForm {
+	@NotBlank(message = "評価を選択してください。")
+	private String reviewStars;
+	
+	private String reviewComment;
+
+}*/
