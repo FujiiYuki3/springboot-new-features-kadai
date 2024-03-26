ebd64f1347bc650e901516b084415fe73d8691d6
FujiiYuki3 <151428461+FujiiYuki3@users.noreply.github.com>
2024-03-26 15:05:37 +0900

    Delete kadai_003/samuraitravel directory

--- kadai_003/samuraitravel/src/main/resources/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
-const stripe = Stripe('pk_test_51OlScLC7ku4vkHH6FxNqQVprxtBvDXSEuBgscp0sZmKkgglXHkouAtnXNjBWXLIJTQnQSsbCPRNXxQXk7DdFcrdi00RA7fKsL2');
-const paymentButton = document.querySelector('#paymentButton');
-
-paymentButton.addEventListener('click', () => {
-	stripe.redirectToCheckout({
-		sessionId: sessionId
-	})
-});
